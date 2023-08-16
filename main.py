04aa577110fbb3227229d0bbca610d028ffb20bc
ccetl <109693935+ccetl@users.noreply.github.com>
2023-08-17 00:21:39 +0200

    deleting

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ action_delay_enabled = True
 action_delay_min = 1
 action_delay_max = 3
 
-
 line_delay_enabled = True
 line_delay_min = 1
 line_delay_max = 3
+
+delete_message = False
 
 
 def focused():
@@ -62,6 +63,25 @@ def run():
 
                 if line_delay_enabled and i != length - 1:
                     time.sleep(random.uniform(line_delay_min, line_delay_max))
+
+        if delete_message:
+            time.sleep(random.uniform(line_delay_min, line_delay_max))
+            pyautogui.press('up')
+            if action_delay_enabled:
+                time.sleep(random.uniform(action_delay_min, action_delay_max))
+            pyautogui.hotkey('ctrl', 'a')
+            if action_delay_enabled:
+                time.sleep(random.uniform(action_delay_min, action_delay_max))
+            pyautogui.press('backspace')
+            if action_delay_enabled:
+                time.sleep(random.uniform(action_delay_min, action_delay_max))
+            if action_delay_enabled:
+                time.sleep(random.uniform(action_delay_min, action_delay_max))
+            pyautogui.press('enter')
+            if action_delay_enabled:
+                time.sleep(random.uniform(action_delay_min, action_delay_max))
+            pyautogui.press('enter')
+            print("deleted message")
 
 
 if __name__ == '__main__':
