6e41c2854f4922615c654aaf21956525d65b0213
ccetl <109693935+ccetl@users.noreply.github.com>
2023-08-16 22:58:38 +0200

    more

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ import time
 import pyautogui
 import win32gui
 
-action_delay_enabled: bool = True
-action_delay_min: float = 1.0
-action_delay_max: float = 3.0
+# If you should encounter issues
+# such as the wrong message gets
+# edited, or it does other weird
+# things, like selecting other
+# fields and your ping to discord
+# is high, consider adjusting
+# the delays to a value which is
+# suitable for your ping.
 
-line_delay_enabled: bool = True
-line_delay_min: float = 1.0
-line_delay_max: float = 3.0
+action_delay_enabled = True
+action_delay_min = 1
+action_delay_max = 3
+
+
+line_delay_enabled = True
+line_delay_min = 1
+line_delay_max = 3
 
 
 def focused():
@@ -27,13 +37,11 @@ def run():
 
     with open('./message.txt', 'r', encoding='utf-8') as file:
         lines = file.readlines()
-        for i in range(len(lines)):
+        length = len(lines)
+        for i in range(length):
             line = lines[i].strip()
             if line:
                 if i == 0:
-                    pyautogui.press('enter')  # ensure the chat is selected
-                    if action_delay_enabled:
-                        time.sleep(random.uniform(action_delay_min, action_delay_max))
                     pyautogui.typewrite(line)
                     if action_delay_enabled:
                         time.sleep(random.uniform(action_delay_min, action_delay_max))
@@ -50,7 +58,9 @@ def run():
                         time.sleep(random.uniform(action_delay_min, action_delay_max))
                     pyautogui.press('enter')
 
-                if line_delay_enabled:
+                print("line " + str(i + 1) + "/" + str(length))
+
+                if line_delay_enabled and i != length - 1:
                     time.sleep(random.uniform(line_delay_min, line_delay_max))
 
 
